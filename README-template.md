982c1ccf5e16b842cd78e6b34930cfa2c51ac5cb
favourachara07 <acharafavour01@gmail.com>
2024-03-08 18:44:25 +0100

    updates

--- README-template.md
+++ README-template.md
@@ -37,15 +37,10 @@ Users should be able to:
 
 ### Screenshot
 
-![](./screenshot.jpg)
+![](./assets/images/Screenshot%20(783).png)
 
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
 
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
 
 ### Links
 
@@ -59,50 +54,86 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - Semantic HTML5 markup
 - CSS custom properties
 - Flexbox
-- CSS Grid
-- Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
+- Vanilla JS
 
-**Note: These are just examples. Delete this note and replace the list above with your own choices**
 
 ### What I learned
 
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+I really learnt alot during this project. I learned while working on the Javascript section of the project. This project involved calculations that can only be achieved by thinking really logically and reasoning. In the html section, I was able to develop more and better understanding of classes and ids, and also the right use of elements. For the CSS, I was able to understand and develop a method fo styling that suits me, i was also able to understand more concepts that i found challenging by applying it in my styles. For the JS section, I developed a better understanding of the DOM, if and else statements, and I also learnt and developed my principle of critical thinking and reasoning
 
 To see how you can add code snippets, see below:
 
 ```html
-<h1>Some HTML code I'm proud of</h1>
+<div class="agecalc">
+        <div class="y"><span class="dash1">--</span> years</div>
+        <div class="m"><span class="dash2">--</span> <span class="per">months</span></div>
+        <div class="d"><span class="dash3">--</span> days</div>
+      </div>
 ```
 ```css
-.proud-of-this-css {
-  color: papayawhip;
+/* the animation of the age numbers */
+.animate{
+    opacity: 0;
+    animation: myfirst 1s ease-in-out 0.6s normal forwards;
+}
+@keyframes myfirst {
+    0% {
+        opacity: 0%;
+    }
+
+    100% {
+        opacity: 100%;
+    }
 }
 ```
 ```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
-}
+// the main calculation behind the age calculator
+let year_total, month_total, day_total;
+
+  if (curMonth > month) {
+    year_total = curYear - year;
+    document.querySelector(".dash1").innerText = year_total;
+  } else {
+    year_total = curYear - year - 1;
+    document.querySelector(".dash1").innerText = year_total;
+  }
+
+  if (month <= curMonth) {
+    month_total = curMonth - month;
+    monthNames();
+    document.querySelector(".dash2").innerText = month_total + 1;
+  } else {
+    month_total = 12 - (month - curMonth);
+    monthNames();
+    document.querySelector(".dash2").innerText = month_total;
+  }
+
+  if (curDay >= day) {
+    day_total = curDay - day;
+    monthNames();
+    document.querySelector(".dash3").innerText = day_total;
+  } else {
+    const tempDate = new Date(curYear, curMonth - 1, 0); // Get the last day of the previous month
+    day_total = tempDate.getDate() - (day - curDay);
+    monthNames();
+    document.querySelector(".dash3").innerText = day_total;
+  }
 ```
 
 If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
 
-**Note: Delete this note and the content within this section and replace with your own learnings.**
+
 
 ### Continued development
 
-Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
+I hope to focus on learning and working with React after i have completed two more projects with html,css and js because working and commanding the dom directly by myself is very tiring. I want that solid foundation before moving on to React. Also, i want to master the act of making my codes shorter and more efficient and cleaner.
 
-**Note: Delete this note and the content within this section and replace with your own plans for continued development.**
+
 
 ### Useful resources
 
-- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
-- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
+- [W3Schools](https://www.w3schools.com/css) - This helped me for the animation of the age numbers reason. I really liked and I have use this platform before and will continue use it going forward.
 
-**Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
 
 ## Author
 
@@ -110,10 +141,17 @@ Use this section to outline areas that you want to continue focusing on in futur
 - Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
 - Twitter - [@favour2207](https://www.twitter.com/yourusername)
 
-**Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
 
 ## Acknowledgments
 
-This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
+I'm actually proud with myself. This is a project that I did independently (95% by myself). Although I got stuck at a point, I did my very best to resolve the dilemma I was in, I solved up to 80% of the problem by myself then I consulted ChatGPT for corrections on what I did.
+This is what ChatGPT corrected me on
+```js
+else {
+    const tempDate = new Date(curYear, curMonth - 1, 0)
+    ; // Get the last day of the previous month
+    day_total = tempDate.getDate() - (day - curDay);
+    monthNames();
+    document.querySelector(".dash3").innerText = day_total;
+```
 
-**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const currentDate = new Date();
 
 // Get various components of the date
 const curYear = currentDate.getFullYear();
-const curMonth = currentDate.getMonth(); // Month is zero-based, so add 1
+const curMonth = currentDate.getMonth() + 1; // Month is zero-based, so add 1
 const curDay = currentDate.getDate();
 const hours = currentDate.getHours();
 const minutes = currentDate.getMinutes();
@@ -14,18 +14,33 @@ console.log(
 );
 
 const reset = () => {
-  document.getElementsByClassName("valid")[0].innerText = "";
-  document.getElementsByClassName("valid1")[0].innerText = "";
-  document.getElementsByClassName("valid2")[0].innerText = "";
+  document.getElementsByClassName("error-d")[0].innerText = "";
+  document.getElementsByClassName("error-m")[0].innerText = "";
+  document.getElementsByClassName("error-y")[0].innerText = "";
 };
 
 function calculateAge() {
-  // Clear previous results
+  const monthNames = () => {
+    // a shorthand for if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30
+    if ([4, 6, 9, 11].includes(month)) {
+      day_total++;
+      month_total--;
+    } else if ([1, 3, 5, 7, 8, 10, 12].includes(month)) {
+      day_total += 2;
+      month_total--;
+    } else {
+      console.log(6);
+    }
+  };
+  document.getElementsByClassName("error")[0].innerText = "";
+  //3 Clear previous results
   document.getElementById("result").innerText = "";
+  document.querySelectorAll(".input_box input").forEach((in_out) => {
+    in_out.classList.remove("outline");
+    // Trigger reflow to ensure the animation class is removed
+    void in_out.offsetWidth;
+  });
 
-  document.querySelector(".dash1");
-  document.querySelector(".dash2");
-  document.querySelector(".dash3");
   //remove class
   document.querySelectorAll(".agecalc span").forEach((span) => {
     span.classList.remove("animate");
@@ -37,7 +52,10 @@ function calculateAge() {
   document.querySelector(".dash2").classList.add("animate");
   document.querySelector(".dash3").classList.add("animate");
 
-  out=document.querySelector(".input_box")
+  outLine = document.querySelector(".input_box");
+  input1 = document.querySelector(".in1");
+  input2 = document.querySelector(".in2");
+  input3 = document.querySelector(".in3");
 
   // Get input values
   const day = parseInt(document.getElementById("day").value);
@@ -51,65 +69,133 @@ function calculateAge() {
 
     return;
   }
-  if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+  if ([4, 6, 9, 11].includes(month) && day > 30) {
     reset();
     document.getElementsByClassName("error")[0].innerText =
       "This month does not have more than 30days";
     return;
   }
-
-  // Check for valid input
+  if (day <= 0) {
+    input1.classList.add("outline");
+    document.getElementsByClassName("error-d")[0].innerText =
+      "Enter a valid day";
+    return;
+  }
+  outLine.classList.remove("outline");
+  // Check for error-d input
   if (isNaN(day) || isNaN(month) || isNaN(year)) {
-    document.getElementsByClassName("valid")[0].innerText = "Enter a day";
-    document.getElementsByClassName("valid1")[0].innerText = "Enter a month";
-    document.getElementsByClassName("valid2")[0].innerText = "Enter a year";
-    out.classList.add("outline");
+    document.getElementsByClassName("error-d")[0].innerText = "Enter a day";
+    document.getElementsByClassName("error-m")[0].innerText = "Enter a month";
+    document.getElementsByClassName("error-y")[0].innerText = "Enter a year";
+    outLine.classList.add("outline");
     return;
   } else {
     reset();
   }
   if (day > 31 || month > 12 || year > curYear) {
-    if (day > 31 || day > curDay) {
-      document.getElementsByClassName("valid")[0].innerText =
+
+    if (year > curYear && day > 31 && month > 12) {
+      document.getElementsByClassName("error-d")[0].innerText =
         "Must be a valid day";
-      out.classList.add("outline");
-    } else {
-      document.getElementsByClassName("valid")[0].innerText = "";
-    }
-    if (month > 12 || month > curMonth) {
-      document.getElementsByClassName("valid1")[0].innerText =
+      document.getElementsByClassName("error-m")[0].innerText =
         "Must be a valid month";
-      out.classList.add("outline");
+      document.getElementsByClassName("error-y")[0].innerText =
+        "Must be a valid year";
+      input3.classList.add("outline");
+      input1.classList.add("outline");
+      input2.classList.add("outline");
+
+      return;
+    } else if (day > 31 && month > 12) {
+      input3.classList.remove("outline");
+      document.getElementsByClassName("error-d")[0].innerText =
+        "Must be a valid day";
+      input1.classList.add("outline");
+      document.getElementsByClassName("error-m")[0].innerText =
+        "Must be a valid month";
+      input2.classList.add("outline");
+      return;
+    } else if (year > curYear && month > 12) {
+      input1.classList.remove("outline");
+      document.getElementsByClassName("error-m")[0].innerText =
+        "Must be a valid month";
+      input2.classList.add("outline");
+      document.getElementsByClassName("error-y")[0].innerText =
+        "Must be a valid year";
+      input3.classList.add("outline");
+      return;
+    } else if (year > curYear && day > 31) {
+      input2.classList.remove("outline");
+      document.getElementsByClassName("error-d")[0].innerText =
+        "Must be a valid day";
+      input1.classList.add("outline");
+      document.getElementsByClassName("error-y")[0].innerText =
+        "Must be a valid year";
+      input3.classList.add("outline");
+      return;
     } else {
-      document.getElementsByClassName("valid1")[0].innerText = "";
+      document.getElementsByClassName("error-d")[0].innerText = "";
+    }
+    if (day > 31) {
+      document.getElementsByClassName("error-d")[0].innerText =
+        "Must be a valid day";
+      input1.classList.add("outline");
+      return;
+    } else {
+      document.getElementsByClassName("error-d")[0].innerText = "";
+    }
+    if (month > 12) {
+      document.getElementsByClassName("error-m")[0].innerText =
+        "Must be a valid month";
+      input2.classList.add("outline");
+      return;
+    } else {
+      document.getElementsByClassName("error-m")[0].innerText = "";
     }
     if (year > curYear) {
-      document.getElementsByClassName("valid2")[0].innerText =
+      document.getElementsByClassName("error-y")[0].innerText =
         "Must be a valid year";
-      out.classList.add("outline");
+      input3.classList.add("outline");
+      return;
     } else {
-      document.getElementsByClassName("valid2")[0].innerText = "";
+      document.getElementsByClassName("error-y")[0].innerText = "";
     }
   }
-  if (day <= curDay || month <= 12 || year <= curYear) {
+  let year_total, month_total, day_total;
+
+  if (curMonth > month) {
     year_total = curYear - year;
-    year_total--;
-
     document.querySelector(".dash1").innerText = year_total;
+  } else {
+    year_total = curYear - year - 1;
+    document.querySelector(".dash1").innerText = year_total;
+  }
 
-    if (month <= curMonth) {
-      month_total = curMonth - month;
-      document.querySelector(".dash2").innerText = month_total;
-    } else {
-      month_total = 12 - Math.abs(month - curMonth);
-      document.querySelector(".dash2").innerText = month_total;
-    }
-    if (curDay > day) {
-      document.querySelector(".dash3").innerText = Math.abs(curDay - day);
-      month_total += 1;
-      document.querySelector(".dash2").innerText = month_total;
-    } else {
-      document.querySelector(".dash3").innerText = 31 - Math.abs(curDay - day);
-    }
+  if (month <= curMonth) {
+    month_total = curMonth - month;
+    monthNames();
+    document.querySelector(".dash2").innerText = month_total + 1;
+  } else {
+    month_total = 12 - (month - curMonth);
+    monthNames();
+    document.querySelector(".dash2").innerText = month_total;
+  }
+
+  if (curDay >= day) {
+    day_total = curDay - day;
+    monthNames();
+    document.querySelector(".dash3").innerText = day_total;
+  } else {
+    const tempDate = new Date(curYear, curMonth - 1, 0); // Get the last day of the previous month
+    day_total = tempDate.getDate() - (day - curDay);
+    monthNames();
+    document.querySelector(".dash3").innerText = day_total;
   }
 }
+const tempDate = new Date(curYear, curMonth - 1, 0); // Get the last day of the previous month
+console.log(tempDate);
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    calculateAge();
+  }
+});
